308dbd13a9e56fbcde61755993a20f2033b72dcb
Ömer Efe DİKİCİ <omerdikici52@gmail.com>
2026-06-05 21:55:00 +0300

    Initial Commit

---
+++ CampusQuest/CampusQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CampusQuestApp.swift
+//  CampusQuest
+//
+//  Created by Ömer Efe DİKİCİ on 5.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct CampusQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CampusQuestUITests/CampusQuestUITestsLaunchTests.swift
@@ -0,0 +1,35 @@
+//
+//  CampusQuestUITestsLaunchTests.swift
+//  CampusQuestUITests
+//
+//  Created by Ömer Efe DİKİCİ on 5.06.2026.
+//
+
+import XCTest
+
+final class CampusQuestUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+        // XCUIAutomation Documentation
+        // https://developer.apple.com/documentation/xcuiautomation
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
